97c3357173b3c027ec8c73639580d862defb83c3
TerrniT <terrnit.megaman@gmail.com>
2023-05-07 00:36:09 +0500

    feat: init frontend & scallfolding svelte project

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center">
+		<h2 class="font-bold">Welcome to Streamify.</h2>
+		<div class="space-y-2 flex justify-center items-center text-center gap-2">
+			<p class="text-[11px] mt-3">Visit github to more information</p>
+			<p><code><a href="https://github.com/rsh-12/streamify/tree/dev">Github</a></code></p>
+		</div>
+	</div>
+</div>
